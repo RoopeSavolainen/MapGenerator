d4c21781d2edb48ec5eab035513e5ce3b7c11c2f
Roope Savolainen <roope.savolainen@gmail.com>
2018-05-18 01:05:12 +0300

    Height map generation

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 import mapgenerator.terrain
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial import voronoi_plot_2d
 
-def main():
-    mesh = mapgenerator.terrain.create_mesh(512)
-    voronoi_plot_2d(mesh)
+def _draw_voronoi(terrain):
+    fig = plt.figure()
+    voronoi_plot_2d(terrain.mesh)
     plt.show()
+
+
+def _draw_heights(terrain):
+    fig = plt.figure()
+    #voronoi_plot_2d(terrain.mesh)
+    ax = fig.add_subplot(111, projection='3d')
+
+    val = list(map(lambda h:
+        (terrain.mesh.points[h[0]][0],
+            terrain.mesh.points[h[0]][1],
+            h[1])
+        , terrain.heights))
+
+    above = filter(lambda v: v[2] > 0.0, val)
+    under = filter(lambda v: v[2] < 0.0, val)
+
+    above_coord = list(zip(*above))
+    under_coord = list(zip(*under))
+
+    ax.scatter(above_coord[0], above_coord[1], above_coord[2], marker='.', c='g')
+    ax.scatter(under_coord[0], under_coord[1], under_coord[2], marker='.', c='b')
+
+    plt.show()
+
+
+def main():
+    terrain = mapgenerator.terrain.Terrain(1024, relaxations=1)
+    _draw_heights(terrain)
 
 if __name__ == '__main__':
     main()
+
--- mapgenerator/terrain.py
+++ mapgenerator/terrain.py
@@ -1,12 +1,16 @@
 import random
-from datetime import datetime
+from time import time
 
 import numpy as np
 import scipy.spatial
 
-import matplotlib.pyplot as plt
+from noise import snoise2
 
 BLUE_NOISE_CANDIDATES = 4
+OCTAVES = 8
+PERSISTENCE = 0.5
+LACUNARITY = 2.0
+
 
 def _sample_blue_noise(P, w=1.0, h=1.0):
     def dist(a,b):
@@ -18,23 +22,46 @@ def _sample_blue_noise(P, w=1.0, h=1.0):
     best = max(candidates, key=lambda p: 0.0 if P.size==0 else dist(p, min(P, key=lambda o: dist(p,o))))
     return np.append(P, [best], axis=0)
 
-def _create_grid(n):
+
+def _create_grid( n):
     P = np.empty([0,2])
     for i in range(n):
         P = _sample_blue_noise(P)
     return P
 
-def create_mesh(n, relaxations=0):
-    points = _create_grid(n)
-    for i in range(relaxations+1):
-        mesh = scipy.spatial.Voronoi(points)
-        points = []
-        for r in mesh.regions:
-            if len(r) > 0:
-                valid = filter(lambda x: x >= 0
-                        and 0.0 <= mesh.vertices[x][0] <= 1.0
-                        and 0.0 <= mesh.vertices[x][1] <= 1.0, r)
-                corners = list(map(lambda x: mesh.vertices[x], valid))
-                mid = sum(corners)/len(corners)
-                points.append(mid)
-    return mesh
+
+class Terrain:
+
+    def __init__(self, n, relaxations=0, seed=None):
+        self.seed = seed if seed is not None else time() % 1000000
+        random.seed(self.seed)
+
+        self.heights = []
+
+        self.generate_mesh(n, relaxations)
+        self.generate_heightmap()
+
+
+    def generate_mesh(self, n, relaxations):
+        points = _create_grid(n)
+
+        for i in range(relaxations+1):
+            mesh = scipy.spatial.Voronoi(points)
+            points = []
+            for r in mesh.regions:
+                if len(r) > 0:
+                    valid = filter(lambda x: x >= 0
+                            and 0.0 <= mesh.vertices[x][0] <= 1.0
+                            and 0.0 <= mesh.vertices[x][1] <= 1.0, r)
+                    corners = list(map(lambda x: mesh.vertices[x], valid))
+                    mid = sum(corners)/len(corners)
+                    points.append(mid)
+        self.mesh = mesh
+
+
+    def generate_heightmap(self):
+        for i in range(len(self.mesh.points)):
+            p = self.mesh.points[i]
+            height = snoise2(p[0], p[1], base=self.seed, octaves=OCTAVES, persistence=PERSISTENCE, lacunarity=LACUNARITY)
+            self.heights.append((i, height))
+
